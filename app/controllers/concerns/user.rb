023c40b03f5164fc808e45501507f626a996d9c8
Vincent Min Tseng <mintseng@berkeley.edu>
2015-11-21 16:34:25 -0800

    created classes in concerns (group, reminder, user) and set up template

---
+++ app/controllers/concerns/user.rb
@@ -0,0 +1,35 @@
+class User
+  def initialize(name, email, birthday)
+    @name = name
+    @email = email
+    @birthday = birthday
+  end
+
+  def setName(name)
+    @name = name
+  end
+
+  def getName()
+    @name
+  end
+
+  def setEmail(email)
+    @email = email
+  end
+
+  def getEmail()
+    @email
+  end
+
+  def addFriend(user)
+  end
+
+  def getFriends()
+  end
+
+  def setGroup()
+  end
+
+  def getGroup()
+  end
+end
