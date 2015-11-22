e745e631510db08e2466c0c486350af587d2aa8b
Vincent Min Tseng <mintseng@berkeley.edu>
2015-11-21 17:22:26 -0800

    added create_reminder to controllers/reminder_controller.rb

--- app/controllers/reminder_controller.rb
+++ app/controllers/reminder_controller.rb
@@ -1,2 +1,12 @@
 class ReminderController < ApplicationController
+  def new
+    @reminders = Reminder.new
+  end
+
+  def create_reminder
+    @note = Reminder.new(text: params[:text], time: params[:time])
+    current_user.reminders << @note
+    redirect_to root_path
+  end
+
 end
