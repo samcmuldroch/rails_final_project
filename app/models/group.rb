1e85c93e8831f35f81b7061854eb95c6b7122a44
Sydney McMuldroch <samcmuldroch@bitbucket.org>
2015-11-21 17:46:59 -0800

    hopefully

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :user
   has_many :users
-  validates :name, presence :true
+  validates :name, presence: true
   
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,4 @@
 class Reminder < ActiveRecord::Base
   belongs_to :user
-  validates :text, presence :true
+  validates :text, presence: true
 end
