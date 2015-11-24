ed95be96c62b54156efa2c598d599d4485571510
Sydney McMuldroch <samcmuldroch@bitbucket.org>
2015-11-23 20:28:24 -0800

    work with the email gem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,8 +13,8 @@ class GroupsController < ApplicationController
 
   def email
   	@group = Group.find(params[:id])
-  	@group.users.each do |user|
-  		Pony.mail(:to => user.email, :from => 'samcmuldroch@berkeley.edu', :subject => 'Important email from' + @group.name, :body => 'body of email here')
-  	end
+  	#@group.users.each do |user|
+  	#	Pony.mail(:to => user.email, :from => 'samcmuldroch@berkeley.edu', :subject => 'Important email from' + @group.name, :body => 'body of email here')
+  	#end
   end
 end
