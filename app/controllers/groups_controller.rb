c5ccb9631679025a83ddcdb634bf1beef3deed72
Sydney McMuldroch <samcmuldroch@bitbucket.org>
2015-11-23 21:06:21 -0800

    email almost working

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,8 +13,11 @@ class GroupsController < ApplicationController
 
   def email
   	@group = Group.find(params[:id])
-  	#@group.users.each do |user|
-  	#	Pony.mail(:to => user.email, :from => 'samcmuldroch@berkeley.edu', :subject => 'Important email from' + @group.name, :body => 'body of email here')
-  	#end
+  	@group.users.each do |user|
+  		Pony.mail(:to => user.email, :from => 'samcmuldroch@berkeley.edu', :subject => 'Important email from' + @group.name, :body => 'body of email here')
+  	end
+    #testing testing
+    Pony.mail(:to => 'mcmuldroch.sydney@gmail.com', :from => 'samcmuldroch@berkeley.edu', :subject => 'Important email from' + @group.name, :body => 'body of email here')
+    redirect_to "http://www.rubyonrails.org"
   end
 end
