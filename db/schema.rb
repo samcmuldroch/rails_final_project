ebc89ca7f909e01b30ac2431777c523596d31698
Vincent Min Tseng <mintseng@berkeley.edu>
2015-11-23 20:16:34 -0800

    fixed seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122022317) do
+ActiveRecord::Schema.define(version: 20151124041229) do
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151122022317) do
     t.datetime "updated_at", null: false
     t.string   "animal"
     t.string   "email"
+    t.integer  "group_id"
   end
 
 end
