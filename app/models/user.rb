4a0325fdc87125a5a22b9cf8518104767bf5a3c4
Vincent Min Tseng <mintseng@berkeley.edu>
2015-12-02 17:23:31 -0800

    sending email now works in console

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,18 @@ class User < ActiveRecord::Base
 #      Pony.mail :to => "vincenttseng06@gmail.com", :via =>:sendmail,
 #      :from => "mintseng@berkeley.edu", :subject => "hi",
 #      :headers => { 'Content-Type' => "text/plain" }, :body => "testing"
-    ActionMailer::Base.mail(from: "vincenttseng06@gmail.com", to: "vincenttseng06@gmail.com", subject: "Test", body: "Test").deliver
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    Authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'rubyfinalproject@gmail.com',
+    password: 'r1u2b3y4'
+}
+    ActionMailer::Base.mail(from: "rubyfinalproject@gmail.com", to: "rubyfinalproject@gmail.com", subject: "Test", body: "Test").deliver
+    
   end
 
 
