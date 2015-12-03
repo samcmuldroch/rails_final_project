3de9acef5ee0ba103d4e542674dc7693160e4a2a
Kathy Jang <kathyjang@berkeley.edu>
2015-12-02 21:01:23 -0800

    sign up works I think

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @users = User.new
+    @user = User.new
   end
   
+  # def create 
+  # 	params.require(:name, :email, :password, :password_confirmation)
+  # 	params.permit(:name)
+  # 	puts params
+
+  # end 
+  def show
+  	@user = User.find(params(:id))
+  end 
 
 end
